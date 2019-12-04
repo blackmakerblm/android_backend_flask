5e72e6a213fa8f037f89e1b281fa582f4b48501c
blackmakerblm <alioubah571@gmail.com>
2019-12-04 04:24:46 +0100

    flask heroku

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 # root
@@ -10,30 +12,7 @@ def index():
     """
     return "This is root!!!!"
 
-# GET
-@app.route('/users/<user>')
-def hello_user(user):
-    """
-    this serves as a demo purpose
-    :param user:
-    :return: str
-    """
-    return "Hello %s!" % user
 
-# POST
-@app.route('/api/post_some_data', methods=['POST'])
-def get_text_prediction():
-    """
-    predicts requested text whether it is ham or spam
-    :return: json
-    """
-    json = request.get_json()
-    print(json)
-    if len(json['text']) == 0:
-        return jsonify({'error': 'invalid input'})
-
-    return jsonify({'you sent this': json['text']})
-    
 # running web app in local machine
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
