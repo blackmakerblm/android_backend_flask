0629131e3c9bbde817b57b4a24fd218215279347
blackmakerblm <48640543+blackmakerblm@users.noreply.github.com>
2019-12-04 05:20:12 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-
-from flask import Flask, request, jsonify
-
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 
 # root
@@ -12,7 +10,30 @@ def index():
     """
     return "This is root!!!!"
 
+    
 
-# running web app in local machine
-if __name__ == '__main__':
+# GET
+@app.route('/users/<user>')
+def hello_user(user):
+    """
+    this serves as a demo purpose
+    :param user:
+    :return: str
+    """
+    return "Hello %s!" % user
+
+# POST
+@app.route('/api/post_some_data', methods=['POST'])
+def get_text_prediction():
+    """
+    predicts requested text whether it is ham or spam
+    :return: json
+    """
+    json = request.get_json()
+    print(json)
+    if len(json['text']) == 0:
+        return jsonify({'error': 'invalid input'})
+
+    return jsonify({'you sent this': json['text']})
+if __name__=='__name__':
     app.run()
